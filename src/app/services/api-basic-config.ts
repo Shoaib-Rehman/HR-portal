9d98ba7e95237914080e1b7f578c14cde905c0a2
Shoaib Rehman <shoaib.rehman832@gmail.com>
2023-08-04 20:27:51 +0500

    working on login integartion

---
+++ src/app/services/api-basic-config.ts
@@ -0,0 +1,5 @@
+
+
+export class APIBasicConfig {
+    base_url: string = "http://35.231.24.34:3000/api/";
+}
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,15 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ILogin } from 'src/app/store/auth/auth.interface';
+import { APIBasicConfig } from '../api-basic-config';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class AuthService extends APIBasicConfig {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    super();
+   }
 
   login(payload: ILogin): any {
-    return "";
+    return this.http.post(`${this.base_url}users/login`, payload);
   }
 }
