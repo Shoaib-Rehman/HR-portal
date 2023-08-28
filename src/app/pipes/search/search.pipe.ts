8a569d139fe0ce93662954572d65d2beee53ffef
Abdul Majid <abdulmajidkohati@gamil.com>
2023-08-28 21:17:59 +0500

    removed debugger

--- src/app/pipes/search/search.pipe.ts
+++ src/app/pipes/search/search.pipe.ts
@@ -6,7 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SearchPipe implements PipeTransform {
   
   transform(items: any[], searchText: string): any[] {
-    debugger
     if (!items) {
       return [];
     }
