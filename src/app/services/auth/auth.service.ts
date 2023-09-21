edb7fad6e96ee0cfe1c48e5c2213597657cb21f5
Shoaib Rehman <shoaib.rehman832@gmail.com>
2023-09-21 16:28:02 +0500

    done logout and navigation on the basis of role

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { IInvalidAuthData, ILogin, ILogout } from 'src/app/store/auth/auth.interface';
+import { IInvalidAuthData, ILogin } from 'src/app/store/auth/auth.interface';
 import { APIBasicConfig } from '../api-basic-config';
 
 @Injectable({
@@ -17,11 +17,7 @@ export class AuthService extends APIBasicConfig {
     return this.http.post<IInvalidAuthData>(`${this.base_url}users/login`, payload);
   }
 
-  // logout(payload: ILogout): Observable<IInvalidAuthData> {
-  //   return this.http.post<IInvalidAuthData>(`${this.base_url}users/login`, payload);
-  // }
-
-  logout(): Observable<IInvalidAuthData> {
-    return this.http.post<IInvalidAuthData>(`${this.base_url}users/login`, {});
+  logout(): Observable<any> {
+    return this.http.post<any>(`${this.base_url}users/logout`, {});
   }
 }
